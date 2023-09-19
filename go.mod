e28dd7bf9f7f18aef6fa56e6969c3ccaf56147c4
Dia2dia552 <144796984+Dia2dia552@users.noreply.github.com>
2023-09-19 13:39:02 +0200

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module SliseArray
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func searchEditorText(editorText []string, searchText string) []string {
+	var results []string
+	for _, line := range editorText {
+		if strings.Contains(line, searchText) {
+			results = append(results, line)
+		}
+	}
+	return results
+}
+func main() {
+	editorText := []string{
+		"перший рядок",
+		"другий рядок",
+		"третій рядок",
+		"четвертий рядок",
+	}
+	fmt.Println("Введіть текст для пошуку: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	searchText := scanner.Text()
+	results := (searchEditorText(editorText, searchText))
+
+	fmt.Printf("Результати пошуку: '%s': /n", searchText)
+	for _, result := range results {
+		fmt.Println(result)
+	}
+}
