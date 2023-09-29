fd2e56db4893c16965391c2ebe5891ae78f51203
Dia2dia552 <144796984+Dia2dia552@users.noreply.github.com>
2023-09-29 11:32:24 +0200

    Update game.go

--- Maps/game.go
+++ Maps/game.go
@@ -44,22 +44,24 @@ func displayBoard() {
 }
 
 func makeMove() {
-	fmt.Print("Enter a position (1-9) to place your symbol: ")
-	var position int
-	_, err := fmt.Scanln(&position)
-	if err != nil {
-		fmt.Println("Invalid input. Please enter a number between 1 and 9.")
-		return
-	}
+	for {
+		fmt.Print("Enter a position (1-9) to place your symbol: ")
+		var position int
+		_, err := fmt.Scanln(&position)
+		if err != nil {
+			fmt.Println("Invalid input. Please enter a number between 1 and 9.")
+			continue
+		}
 
-	if position < 1 || position > 9 || board[position] != " " {
-		fmt.Println("Invalid move. Try again.")
-		return
-	}
+		if position < 1 || position > 9 || board[position] != " " {
+			fmt.Println("Invalid move. Try again.")
+			continue
+		}
 
-	board[position] = currentPlayer
+		board[position] = currentPlayer
+		break
+	}
 }
-
 func switchPlayer() {
 	if currentPlayer == "X" {
 		currentPlayer = "O"
@@ -67,25 +69,6 @@ func switchPlayer() {
 		currentPlayer = "X"
 	}
 }
-
-func checkWinner() {
-	if checkLine(1, 2, 3) || checkLine(4, 5, 6) || checkLine(7, 8, 9) ||
-		checkLine(1, 4, 7) || checkLine(2, 5, 8) || checkLine(3, 6, 9) ||
-		checkLine(1, 5, 9) || checkLine(3, 5, 7) {
-		displayBoard()
-		fmt.Println("Player", currentPlayer, "wins!")
-		gameOver = true
-	} else if boardIsFull() {
-		displayBoard()
-		fmt.Println("It's a draw!")
-		gameOver = true
-	}
-}
-
-func checkLine(a, b, c int) bool {
-	return board[a] != " " && board[a] == board[b] && board[b] == board[c]
-}
-
 func boardIsFull() bool {
 	for i := 1; i <= 9; i++ {
 		if board[i] == " " {
@@ -93,4 +76,33 @@ func boardIsFull() bool {
 		}
 	}
 	return true
+}
+
+var winCombinations = [8][3]int{
+	{1, 2, 3},
+	{4, 5, 6},
+	{7, 8, 9},
+	{1, 4, 7},
+	{2, 5, 8},
+	{3, 6, 9},
+	{1, 5, 9},
+	{3, 5, 7},
+}
+
+func checkWinner() {
+	for _, combo := range winCombinations {
+		a, b, c := combo[0], combo[1], combo[2]
+		if board[a] != " " && board[a] == board[b] && board[b] == board[c] {
+			displayBoard()
+			fmt.Println("Player", currentPlayer, "wins!")
+			gameOver = true
+			return
+		}
+	}
+
+	if boardIsFull() {
+		displayBoard()
+		fmt.Println("It's a draw!")
+		gameOver = true
+	}
 }
