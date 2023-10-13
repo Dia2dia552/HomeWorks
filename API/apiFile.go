11c4c87f5af11785d502a1229ba6768893403db3
Dia2dia552 <144796984+Dia2dia552@users.noreply.github.com>
2023-10-13 17:23:31 +0200

    update

---
+++ API/apiFile.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Task struct {
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"complited" `
+	Date      string `json:"date"`
+}
+
+var tasks = []Task{
+	{ID: "1", Item: "Clean a room", Completed: false, Date: "20.02.2020"},
+	{ID: "2", Item: "Go shopping", Completed: false, Date: "22.02.2020"},
+	{ID: "3", Item: "Cook dinner", Completed: false, Date: "22.02.2020"},
+}
+
+func getTasks(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, tasks)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/tasks", getTasks)
+	err := router.Run("localhost:9090")
+	if err != nil {
+		return
+	}
+}
